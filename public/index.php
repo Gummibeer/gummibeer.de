29cb0e3e782a330836b26e300d84477c0838cc66
Tom Witkowski <dev.gummibeer@gmail.com>
2017-02-14 17:03:46 +0100

    remove a customer from pages

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ $app = require realpath(__DIR__ . '/../app.php');
 if (!($app instanceof Application)) {
     throw new RuntimeException('Failed to create app instance.');
 }
-$app['version'] = $app['debug'] ? time() : '1.3.10';
+$app['version'] = $app['debug'] ? time() : '1.3.11';
 $app['cache_path'] = realpath(__DIR__ . '/../cache');
 
 function title($title = '')
