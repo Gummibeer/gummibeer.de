514babf37eb41b76c7b3a63119a6c4126e580954
Tom Witkowski <dev.gummibeer@gmail.com>
2017-09-25 13:52:06 +0200

    add consulting themes

--- resources/views/pages/home/consulting.blade.php
+++ resources/views/pages/home/consulting.blade.php
@@ -13,7 +13,7 @@
                     </h3>
                 </div>
                 <div class="panel-body">
-                    I support them with local marketing ideas and help to get an online strategy.
+                    Marketing, Social-Media, Online-Strategy
                 </div>
                 <div class="panel-footer">
                     <div class="row">
@@ -45,7 +45,7 @@
                     </h3>
                 </div>
                 <div class="panel-body">
-                    I share my social-media and photography knowledge.
+                    Social-Media, Photography
                 </div>
                 <div class="panel-footer">
                     <div class="row">
@@ -56,6 +56,38 @@
                         </div>
                         <div class="col-xs-6 text-center">
                             <a href="https://instagram.com/pandorya.photo" target="_blank" rel="noopener">
+                                <i class="icon fa-instagram fa-2x"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-xs-12 masonry-item">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title clearfix">
+                        Maximko
+                        <i class="icon fa-paint-brush pull-right"></i>
+                    </h3>
+                </div>
+                <div class="panel-body">
+                    Social-Media, Shop, Online-Strategy
+                </div>
+                <div class="panel-footer">
+                    <div class="row">
+                        <div class="col-xs-4 text-center">
+                            <a href="http://maximko.de" target="_blank" rel="noopener">
+                                <i class="icon fa-globe fa-2x"></i>
+                            </a>
+                        </div>
+                        <div class="col-xs-4 text-center">
+                            <a href="https://facebook.com/maximkoart" target="_blank" rel="noopener">
+                                <i class="icon fa-facebook fa-2x"></i>
+                            </a>
+                        </div>
+                        <div class="col-xs-4 text-center">
+                            <a href="https://instagram.com/maximkoart" target="_blank" rel="noopener">
                                 <i class="icon fa-instagram fa-2x"></i>
                             </a>
                         </div>
