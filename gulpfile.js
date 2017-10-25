44f5f0e7e4c59fe930b5ad6e1f0873ddc57c8902
Tom Witkowski <dev.gummibeer@gmail.com>
2017-10-25 10:51:08 +0200

    write version after asset update

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
+var fs = require('fs');
 var gulp = require('gulp');
 var less = require('gulp-less');
-var path = require('path');
 var sourcemaps = require('gulp-sourcemaps');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
@@ -9,17 +9,18 @@ var concat = require('gulp-concat');
 gulp.task('default', ['watch']);
 
 gulp.task('less', function () {
-    return gulp.src('./resources/assets/less/styles.less')
+    gulp.src('./resources/assets/less/styles.less')
         .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./public/css'));
+    gulp.run('touch');
 });
 
 gulp.task('scripts', function() {
-    return gulp.src([
+    gulp.src([
         './resources/assets/js/jquery.min.js',
         './resources/assets/js/jquery.bootstrap.min.js',
         './resources/assets/js/jquery.smooth-scroll.min.js',
@@ -32,8 +33,13 @@ gulp.task('scripts', function() {
     ])
         .pipe(concat('scripts.min.js'))
         .pipe(gulp.dest('./public/js'));
+    gulp.run('touch');
 });
 
 gulp.task('watch', function () {
     gulp.watch('./resources/assets/less/**/*.less', ['less']);
+});
+
+gulp.task('touch', function(callback) {
+    fs.writeFile('./.version', Date.now(), callback);
 });
