6422b59c104d97626034b9668cd847882e4c7ac6
Tom Witkowski <dev.gummibeer@gmail.com>
2018-02-16 19:21:29 +0100

    fix php cs: app/Exceptions/Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,9 +3,9 @@
 namespace App\Exceptions;
 
 use Exception;
-use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException as SymfonyHttpException;
 
@@ -28,7 +28,7 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $e
+     * @param  \Exception $e
      * @return void
      */
     public function report(Exception $e)
@@ -39,17 +39,17 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $e
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Exception $e
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $e)
     {
-        if($e instanceof SymfonyHttpException) {
+        if ($e instanceof SymfonyHttpException) {
             $e = HttpException::make($e);
             return response(view('errors.error')->with([
                 'exception' => $e,
-                'title' => title($e->getStatusCode() .' ' . $e->getStatusText()),
+                'title' => title($e->getStatusCode() . ' ' . $e->getStatusText()),
             ]), $e->getStatusCode());
         }
         return parent::render($request, $e);
