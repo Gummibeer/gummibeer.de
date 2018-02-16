c66c1b674fa77e1d00eb471cd31b50cd8d6176fe
Tom Witkowski <dev.gummibeer@gmail.com>
2018-02-16 19:21:29 +0100

    fix php cs: app/helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -1,18 +1,21 @@
 <?php
-if(!function_exists('asset')) {
-    function asset($path) {
-        return url($path.'?v='.file_get_contents(base_path('.version')));
+if (!function_exists('asset')) {
+    function asset($path)
+    {
+        return url($path . '?v=' . file_get_contents(base_path('.version')));
     }
 }
 
-if(!function_exists('title')) {
-    function title($title = '') {
+if (!function_exists('title')) {
+    function title($title = '')
+    {
         return implode(' | ', array_filter([trim($title), 'Tom Witkowski']));
     }
 }
 
-if(!function_exists('selected_countries')) {
-    function selected_countries() {
+if (!function_exists('selected_countries')) {
+    function selected_countries()
+    {
         $allCountries = require resource_path('all_countries.php');
         $selected = json_decode(file_get_contents(resource_path('selected_countries.json')), true);
         $countries = array_intersect_key($allCountries, array_combine($selected, $selected));
