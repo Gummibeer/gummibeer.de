6895bd1ccf5dc130530c1fb0f8b11bfd6158696b
Tom Witkowski <dev.gummibeer@gmail.com>
2018-02-28 15:23:06 +0100

    add The Great Bubble Barrier to charity

--- resources/views/pages/home/charity.blade.php
+++ resources/views/pages/home/charity.blade.php
@@ -303,5 +303,34 @@
                 </div>
             </div>
         </div>
+        <div class="col-md-4 col-xs-12 masonry-item">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title clearfix">
+                        The Great Bubble Barrier
+                        <i class="icon fa-money pull-right"></i>
+                    </h3>
+                </div>
+                <div class="panel-footer">
+                    <div class="row">
+                        <div class="col-xs-4 text-center">
+                            <a href="http://thegreatbubblebarrier.com" target="_blank" rel="noopener noreferrer">
+                                <i class="icon fa-globe fa-2x"></i>
+                            </a>
+                        </div>
+                        <div class="col-xs-4 text-center">
+                            <a href="https://facebook.com/TheGreatBubbleBarrier" target="_blank" rel="noopener noreferrer">
+                                <i class="icon fa-facebook fa-2x"></i>
+                            </a>
+                        </div>
+                        <div class="col-xs-4 text-center">
+                            <a href="https://instagram.com/thegreatbubblebarrier" target="_blank" rel="noopener noreferrer">
+                                <i class="icon fa-instagram fa-2x"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
