fcd10c4903dadf78f12d062f280be35440c55919
Tom Witkowski <dev.gummibeer@gmail.com>
2018-03-29 11:36:19 +0200

    add sea shepherd

--- resources/views/pages/home/charity.blade.php
+++ resources/views/pages/home/charity.blade.php
@@ -332,5 +332,34 @@
                 </div>
             </div>
         </div>
+        <div class="col-md-4 col-xs-12 masonry-item">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title clearfix">
+                        Sea Shepherd
+                        <i class="icon fa-money pull-right"></i>
+                    </h3>
+                </div>
+                <div class="panel-footer">
+                    <div class="row">
+                        <div class="col-xs-4 text-center">
+                            <a href="https://seashepherd.org" target="_blank" rel="noopener noreferrer">
+                                <i class="icon fa-globe fa-2x"></i>
+                            </a>
+                        </div>
+                        <div class="col-xs-4 text-center">
+                            <a href="https://facebook.com/seashepherdconservationsociety" target="_blank" rel="noopener noreferrer">
+                                <i class="icon fa-facebook fa-2x"></i>
+                            </a>
+                        </div>
+                        <div class="col-xs-4 text-center">
+                            <a href="https://instagram.com/seashepherdsscs" target="_blank" rel="noopener noreferrer">
+                                <i class="icon fa-instagram fa-2x"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
