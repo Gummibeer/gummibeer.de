45843c393bcfa459308ed1dcd3f36ee0e7922845
Tom Witkowski <dev.gummibeer@gmail.com>
2018-03-29 11:36:33 +0200

    add dogfit

--- resources/views/pages/home/pages.blade.php
+++ resources/views/pages/home/pages.blade.php
@@ -149,5 +149,32 @@
                 </div>
             </div>
         </div>
+        <div class="col-md-4 col-xs-12 masonry-item">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title clearfix">
+                        Dogfit - Fitness mit Hund
+                        <i class="icon fa-paw pull-right"></i>
+                    </h3>
+                </div>
+                <div class="panel-body">
+                    Human & Dog fitness training.
+                </div>
+                <div class="panel-footer">
+                    <div class="row">
+                        <div class="col-xs-6 text-center">
+                            <a href="http://dogfit-fitness.de" target="_blank" rel="noopener noreferrer">
+                                <i class="icon fa-globe fa-2x"></i>
+                            </a>
+                        </div>
+                        <div class="col-xs-6 text-center">
+                            <a href="https://facebook.com/DogfitFitnessMitHund" target="_blank" rel="noopener noreferrer">
+                                <i class="icon fa-facebook fa-2x"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
