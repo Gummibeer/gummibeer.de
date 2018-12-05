5d4434c43959c583b3ab86e76b451c61186e7612
Gummibeer <dev.gummibeer@gmail.com>
2018-12-05 12:58:35 +0100

    add villa psili

--- resources/views/pages/home/pages.blade.php
+++ resources/views/pages/home/pages.blade.php
@@ -245,5 +245,32 @@
                 </div>
             </div>
         </div>
+        <div class="col-md-4 col-xs-12 masonry-item">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title clearfix">
+                        Villa Psili & Villa Astoria
+                        <i class="icon far fa-bed pull-right"></i>
+                    </h3>
+                </div>
+                <div class="panel-body">
+                    Holiday home on Crete.
+                </div>
+                <div class="panel-footer">
+                    <div class="row">
+                        <div class="col-xs-6 text-center">
+                            <a href="https://villa-psili.holiday" target="_blank" rel="noopener noreferrer">
+                                <i class="icon far fa-globe fa-2x"></i>
+                            </a>
+                        </div>
+                        <div class="col-xs-6 text-center">
+                            <a href="https://google.com/maps?cid=4888678822266930759" target="_blank" rel="noopener noreferrer">
+                                <i class="icon far fa-map-marker fa-2x"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
