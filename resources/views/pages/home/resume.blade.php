889559f4223dcb4e640f7be7a764375855466ce5
Gummibeer <dev.gummibeer@gmail.com>
2019-03-11 14:46:38 +0100

    add spatie and links in resume

--- resources/views/pages/home/resume.blade.php
+++ resources/views/pages/home/resume.blade.php
@@ -12,6 +12,16 @@
             </h3>
             <strong>Founder & Developer</strong>
             <time>2016 - now</time>
+            <a href="https://github.com/Astrotomic" target="_blank" rel="noopener noreferrer">https://github.com/Astrotomic</a>
+        </li>
+        <li class="active">
+            <h3>
+                <i class="icon far fa-unicorn"></i>
+                Spatie
+            </h3>
+            <strong>Open Source Developer</strong>
+            <time>2015 - now</time>
+            <a href="https://spatie.be" target="_blank" rel="noopener noreferrer">https://spatie.be</a>
         </li>
         <li class="active">
             <h3>
@@ -20,22 +30,25 @@
             </h3>
             <strong>Backend Developer</strong>
             <time>2017 - now</time>
+            <a href="https://www.even-on-sunday.com" target="_blank" rel="noopener noreferrer">https://www.even-on-sunday.com</a>
         </li>
         <li>
             <h3>
                 <i class="icon far fa-briefcase"></i>
-                Absolute Software GmbH
+                Absolute Software
             </h3>
             <strong>Junior Software Developer</strong>
             <time>2015 - 2017</time>
+            <a href="https://absolute.de" target="_blank" rel="noopener noreferrer">https://absolute.de</a>
         </li>
         <li>
             <h3>
                 <i class="icon far fa-graduation-cap"></i>
-                Medienwerft GmbH
+                Medienwerft
             </h3>
             <strong>Training: Software Developer</strong>
             <time>2013 - 2015</time>
+            <a href="https://medienwerft.de" target="_blank" rel="noopener noreferrer">https://medienwerft.de</a>
         </li>
         <li class="unimportant">
             <h3>
