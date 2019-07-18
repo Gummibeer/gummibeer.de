c7b324e38355ec8369b987cb5a0594c9e4c514da
Gummibeer <dev.gummibeer@gmail.com>
2019-07-18 16:11:34 +0200

    upgrade to gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -7,23 +7,23 @@ var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var googleWebFonts = require('gulp-google-webfonts');
 
-gulp.task('default', ['less', 'scripts', 'googlefonts']);
-
-gulp.task('less', function () {
-    gulp.src('./resources/assets/less/styles.less')
+function task_less() {
+    return gulp.src('./resources/assets/less/styles.less')
         .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./public/css'));
-    gulp.src(['./node_modules/@fortawesome/fontawesome-pro-webfonts/webfonts/**/*'])
-        .pipe(gulp.dest('public/fonts'));
-    gulp.run('touch');
-});
+}
 
-gulp.task('scripts', function() {
-    gulp.src([
+function task_fonts() {
+    return gulp.src(['./node_modules/@fortawesome/fontawesome-pro-webfonts/webfonts/**/*'])
+        .pipe(gulp.dest('public/fonts'));
+}
+
+function task_scripts() {
+    return gulp.src([
         './resources/assets/js/jquery.min.js',
         './resources/assets/js/jquery.bootstrap.min.js',
         './resources/assets/js/jquery.smooth-scroll.min.js',
@@ -35,20 +35,24 @@ gulp.task('scripts', function() {
     ])
         .pipe(concat('scripts.min.js'))
         .pipe(gulp.dest('./public/js'));
-    gulp.run('touch');
-});
+}
 
-gulp.task('googlefonts', function () {
-    gulp.src('./fonts.list')
+function task_googlefonts() {
+    return gulp.src('./fonts.list')
         .pipe(googleWebFonts({}))
         .pipe(gulp.dest('public/css'));
-    gulp.run('touch');
-});
+}
 
-gulp.task('watch', function () {
-    gulp.watch('./resources/assets/less/**/*.less', ['less']);
-});
+function task_watch() {
+    return gulp.watch('./resources/assets/less/**/*.less', ['less']);
+}
 
-gulp.task('touch', function(callback) {
+function touch(callback) {
     fs.writeFile('./.version', Date.now(), callback);
-});
+}
+
+exports.less = gulp.parallel(task_less, task_fonts, touch);
+exports.scripts = gulp.parallel(task_scripts, touch);
+exports.googlefonts = gulp.parallel(task_googlefonts, touch);
+exports.watch = task_watch;
+exports.default = gulp.parallel(task_less, task_fonts, task_scripts, task_googlefonts, touch);
