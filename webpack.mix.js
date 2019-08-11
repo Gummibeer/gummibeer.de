95183a95694b12ab2146d3fae0f6b604141ec54e
Gummibeer <dev.gummibeer@gmail.com>
2019-08-11 19:41:10 +0200

    switch to webpack mix instead of gulp

---
+++ webpack.mix.js
@@ -0,0 +1,48 @@
+let mix = require('laravel-mix');
+require('laravel-mix-sri');
+const GoogleFontsPlugin = require('google-fonts-webpack-plugin')
+
+mix
+    .less('resources/assets/less/styles.less', 'public/css/styles.min.css')
+    .combine([
+        './resources/assets/js/jquery.min.js',
+        './resources/assets/js/jquery.bootstrap.min.js',
+        './resources/assets/js/jquery.smooth-scroll.min.js',
+        './resources/assets/js/jquery.count-to.min.js',
+        './resources/assets/js/jquery.masonry.min.js',
+        './resources/assets/js/jquery.vectormap.min.js',
+        './resources/assets/js/jquery.vectormap.europe_mill.min.js',
+        './resources/assets/js/jquery.main.js',
+    ], 'public/js/scripts.min.js')
+    .webpackConfig({
+        plugins: [
+            new GoogleFontsPlugin({
+                filename: '/css/fonts.css',
+                path: '/fonts/vendor/googlefonts/',
+                fonts: [
+                    {
+                        family: 'Lato',
+                        variants: [
+                            '300',
+                            '400',
+                            '700',
+                        ]
+                    },
+                    {
+                        family: 'Montserrat',
+                        variants: [
+                            '700',
+                        ]
+                    },
+                    {
+                        family: 'Raleway',
+                        variants: [
+                            '400',
+                            '700',
+                        ]
+                    }
+                ]
+            }),
+        ],
+    })
+;
