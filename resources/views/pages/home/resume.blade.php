663d0ec0cb4cc3dea8ff1bf508e389e939882952
Gummibeer <dev.gummibeer@gmail.com>
2019-10-07 12:14:28 +0200

    add hacktoberfest 2019

--- resources/views/pages/home/resume.blade.php
+++ resources/views/pages/home/resume.blade.php
@@ -78,6 +78,13 @@
         <li class="unimportant">
             <h3>
                 <i class="icon far fa-calendar-alt"></i>
+                Hacktoberfest 2019
+            </h3>
+            <strong>Open Source Developer & Maintainer</strong>
+        </li>
+        <li class="unimportant">
+            <h3>
+                <i class="icon far fa-calendar-alt"></i>
                 Web Summit 2018
             </h3>
             <strong>Open Source Developer Attendee</strong>
