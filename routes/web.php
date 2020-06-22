00ed13cfc813be93f793f89103bf16daacd4073c
Gummibeer <Gummibeer@users.noreply.github.com>
2020-06-22 16:21:18 +0000

    fix php cs

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\Blog;
 use App\Http\Controllers\HomeController;
 use App\Http\Middleware\Paginated;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Steein\Robots\Robots;
 
