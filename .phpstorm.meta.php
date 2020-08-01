bb5d631a57b5823001279276af358b70c711e1af
Tom Witkowski <dev.gummibeer@gmail.com>
2020-08-01 13:46:48 +0200

    remove custom phpstorm.meta

--- .phpstorm.meta.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Illuminate\Support {
-    abstract class Collection {
-        abstract public function paginate(?int $page = null, int $perPage = 3, array $options = []): \App\Services\Paginator;
-    }
-}
