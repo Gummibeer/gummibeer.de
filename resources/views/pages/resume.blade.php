b63e904db49405ccae108577e3b819de1d966f7b
Tom Witkowski <dev.gummibeer@gmail.com>
2020-09-25 23:32:30 +0200

    fix hacktoberfest margin

--- resources/views/pages/resume.blade.php
+++ resources/views/pages/resume.blade.php
@@ -67,7 +67,7 @@
     </x-section>
 
     <x-section>
-        <div class="sm:px-4 md:px-0 mx-auto w-full sm:max-w-screen-sm md:max-w-screen-md prose md:prose-lg lg:prose-xl mb-4">
+        <div class="sm:px-4 md:px-0 mx-auto w-full sm:max-w-screen-sm md:max-w-screen-md prose md:prose-lg lg:prose-xl mb-8">
             <h2>Hacktoberfest</h2>
             <p>
                 A monthlong celebration of open source software hosted by DigitalOcean, Intel and DEV.
