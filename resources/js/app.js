892d08c86756adeadd71e5da13a6de02ab545153
Tom Witkowski <dev.gummibeer@gmail.com>
2020-12-11 20:42:46 +0100

    add env example to human readable intervals post

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ require("alpinejs");
 const Prism = require("prismjs");
 require("prismjs/components/prism-markup-templating");
 require("prismjs/components/prism-php");
+require("prismjs/components/prism-ini");
 require("prismjs/plugins/line-numbers/prism-line-numbers");
 Prism.highlightAll();
 
