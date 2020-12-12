768079ba503d7b389d2d720443a0949616a4922d
Tom Witkowski <dev.gummibeer@gmail.com>
2020-12-12 13:52:14 +0100

    fix mobile menu

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,21 +1,32 @@
 <header class="bg-white dark:bg-night-10 shadow sticky top-0 left-0 right-0 z-10">
-    <nav class="px-4 md:px-8 lg:px-10 xl:px-12 flex flex-wrap md:flex-no-wrap md:justify-between" x-data="{show: false}" :class="{'h-screen': show}">
-        <div class="flex flex-grow md:flex-auto items-center">
-            <a href="{{ route('home') }}" class="inline-block font-logo text-2xl lg:text-3xl leading-none tracking-wider py-4 whitespace-no-wrap">
-                Tom Witkowski
-            </a>
-        </div>
+    <nav
+        class="px-4 md:px-8 lg:px-10 xl:px-12 flex flex-col md:flex-row flex-no-wrap md:justify-between"
+        x-data="{show: false}"
+        :class="{'h-screen': show}"
+    >
+        <div class="flex-shrink flex flex-row w-full">
+            <div class="flex flex-grow md:flex-auto items-center">
+                <a href="{{ route('home') }}" class="inline-block font-logo text-2xl lg:text-3xl leading-none tracking-wider py-4 whitespace-no-wrap">
+                    Tom Witkowski
+                </a>
+            </div>
 
-        <div class="my-1 md:hidden" :class="{'mm-wrapper_opened': show}">
-            <button class="block md:hidden mburger mburger--squeeze" @click="show = !show" type="button" aria-controls="menu-list" :aria-expanded="show ? 'true' : 'false'" aria-label="toggle main menu visibility">
-                <b></b>
-                <b></b>
-                <b></b>
-                <span class="sr-only">main menu visibility toggle</span>
-            </button>
+            <div class="my-1 md:hidden" :class="{'mm-wrapper_opened': show}">
+                <button
+                    type="button"
+                    class="block md:hidden mburger mburger--squeeze"
+                    @click="show = !show"
+                    aria-controls="menu-list"
+                    :aria-expanded="show ? 'true' : 'false'"
+                    aria-label="toggle main menu visibility"
+                >
+                    <b></b>
+                    <b></b>
+                    <b></b>
+                    <span class="sr-only">main menu visibility toggle</span>
+                </button>
+            </div>
         </div>
-
-        <div class="h-0 flex-basis-full md:hidden"></div>
 
         <ul class="md:flex w-full md:w-auto list-none md:space-x-2 lg:space-x-4" :class="{'flex flex-col': show, 'hidden': !show}" id="menu-list">
             @foreach([
