83eaf1320f35d4c988ec6770825360df3825b578
Tom Witkowski <dev.gummibeer@gmail.com>
2020-12-17 19:39:20 +0100

    paginate every 6 elements

--- app/Macros/CollectionMixin.php
+++ app/Macros/CollectionMixin.php
@@ -11,7 +11,7 @@ final class CollectionMixin
 {
     public function paginate(): Closure
     {
-        return function (?int $page = null, int $perPage = 3, array $options = []): Paginator {
+        return function (?int $page = null, int $perPage = 6, array $options = []): Paginator {
             return Paginator::make($this, $perPage, $page, $options);
         };
     }
