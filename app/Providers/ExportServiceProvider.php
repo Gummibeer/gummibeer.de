183c54fdba7bb50a638aed9515f8401853b283e0
Jules Janssen <492036+julesjanssen@users.noreply.github.com>
2020-12-27 17:48:36 +0100

    fix feed exports

--- app/Providers/ExportServiceProvider.php
+++ app/Providers/ExportServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Author;
 use App\Category;
 use App\Post;
 use Illuminate\Support\ServiceProvider;
@@ -20,30 +21,27 @@ class ExportServiceProvider extends ServiceProvider
             $this->app->booted(function (): void {
                 $exporter = $this->app->make(Exporter::class);
 
-                Post::all()
-                    ->each(function (Post $post) use ($exporter): void {
-                        $exporter
-                            ->urls(route('blog.year.index', ['year' => $post->date->year]))
-                            ->urls([
-                                $post->author->url,
-                                route('blog.author.feed', ['author' => $post->author, 'format' => 'rss']),
-                                route('blog.author.feed', ['author' => $post->author, 'format' => 'atom']),
-                            ])
-                            ->urls(
-                                collect($post->categories())
-                                    ->map(function (Category $category) {
-                                        return [
-                                            $category->url,
-                                            route('blog.category.feed', ['category' => $category, 'format' => 'rss']),
-                                            route('blog.category.feed', ['category' => $category, 'format' => 'atom']),
-                                        ];
-                                    })
-                                    ->flatten()
-                                    ->all()
-                            );
-                    });
-            }
-            );
+                $exporter->urls([
+                    route('blog.feed', ['format' => 'rss']),
+                    route('blog.feed', ['format' => 'atom']),
+                ]);
+
+                Category::all()->each(fn (Category $category) => $exporter->urls([
+                    $category->url,
+                    route('blog.category.feed', ['category' => $category, 'format' => 'rss']),
+                    route('blog.category.feed', ['category' => $category, 'format' => 'atom']),
+                ]));
+
+                Author::all()->each(fn (Author $author) => $exporter->urls([
+                    $author->url,
+                    route('blog.author.feed', ['author' => $author, 'format' => 'rss']),
+                    route('blog.author.feed', ['author' => $author, 'format' => 'atom']),
+                ]));
+
+                Post::all()->each(fn (Post $post) => $exporter->urls(
+                    route('blog.year.index', ['year' => $post->date->year])
+                ));
+            });
         }
     }
 }
